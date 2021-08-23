f0e94c367e08add34c345669e42f9731de96b819
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-08-23 18:00:15 +0200

    Eureka server created

---
+++ src/test/java/com/guru/sfg/brewery/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.sfg.brewery.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
